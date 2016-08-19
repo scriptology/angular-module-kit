4e81958fc1215742f150ebb0271f9bfb5da6a0f0
Andrey Soloviev <andreysoloweb@gmail.com>
2016-08-19 15:04:23 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+var webpack = require("webpack");
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+//var BowerWebpackPlugin = require('bower-webpack-plugin');
+//var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const NODE_ENV = process.env.NODE_ENV || 'dev';
+
+var config = {
+    entry: {
+        app: ['./src/index.js']
+    },
+    //devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/',
+        chunkFilename: 'js/bundle_'+ NODE_ENV +'.[name].js',
+        filename: 'js/bundle_'+ NODE_ENV +'.js'
+    },
+    resolve: {
+        root: __dirname + '/src/'
+        //alias: {
+        //    'src': __dirname + '/dist/'
+        //}
+
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    //watch: true,
+    //devtool: "source-map", // or "inline-source-map"
+    module: {
+        noParse: [],
+        loaders: [
+
+            //{ test: /\.png$/, loader: "url-loader?limit=100000" },
+
+            // JS
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'ng-annotate!babel'
+            },
+
+            // HTML
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $:      "jquery",
+            jQuery: "jquery"
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            mangle: false,
+            compress: { warnings: false }
+        }),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        }),
+
+        new HtmlWebpackPlugin({
+            template: '!!pug!src/index.pug'
+        })
+    ]
+};
+
+module.exports = config;
